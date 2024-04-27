cf912f2f9237c72037dc271afe813372f11aa850
Shuga8 <lotocharles8@gmail.com>
2024-04-28 00:20:35 +0100

    persist

---
+++ app.js
@@ -0,0 +1,19 @@
+// Importing required modules
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+  console.log(`serving on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile();
+});
