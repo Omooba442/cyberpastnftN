6691eb7d33ae3cbca9b3563581950dcbe8183fae
Shuga8 <lotocharles8@gmail.com>
2024-04-28 09:49:12 +0100

    reverting

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.post("/contact", sendMail);
 app.post("/subscribe", subscribeMail);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
